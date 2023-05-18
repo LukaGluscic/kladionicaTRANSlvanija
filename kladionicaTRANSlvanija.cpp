c9a1e6c1a07aa44fc67471602017ce5aec5fc2fa
LukaGluscic <bozic1387@gmail.com>
2023-05-18 18:21:28 +0200

    Update kladionicaTRANSlvanija.cpp

--- kladionicaTRANSlvanija.cpp
+++ kladionicaTRANSlvanija.cpp
@@ -2,6 +2,7 @@
 #include<time.h>
 #include<algorithm>
 #include<string>
+#include<cctype>
 using namespace std;
 
 void pause() {
@@ -13,40 +14,57 @@ void pause() {
 
 int main()
 {
-	int izbor;
-    string * Imeprezime = new string[1000];
-    double ulog;
+    int izbor;
+    string* Imeprezime = new string[1000];
+    double* ulog = new double[1000];
+    int ovisnik = 0;
+    string odgovor;
     while (1) {
-        system("clear"); 
-        cout << "Dobrodosli u kladionicu TRANSilvanija\n";
+        cout << "Dobrodosli u kladionicu TRANSilvanija\n" << endl;
         cout << "1. Registracija" << endl;
         cout << "2. Stanje racuna" << endl;
-        cout << "3. Utrke èetveronožnih trèkala "<< endl;
+        cout << "3. Uplata  " << endl;
         cout << "4. Nogometne utakmice" << endl;
-        cout << "5. brisanje raèuna" << endl;
-        cout << "6. ispiši sortirano po prezimenu i imenu" << endl;
+        cout << "5. Utrke cetveronoznih trckala" << endl;
+        cout << "6. " << endl;
         cout << "7. izlaz iz programa" << endl;
         cout << "8. " << endl;
         cout << "vas odabir je: ";
         cin >> izbor;
-        if (izbor == 1) 
+        if (izbor == 1)
         {
             cout << "Unesite vase ime i prezime " << endl;
             cin.ignore();
             getline(cin, *Imeprezime);
-            cout << "unesite kolièinu novca kojeg želite unesti: ";
-            cin >> ulog;
+            cout << "unesite kolicinu novca kojeg zelite unesti: " << endl;
+            cin >> ulog[ovisnik];
+            ovisnik++;
         }
-     if (izbor == 2) {
-            cout << "Vaše stanje raèuna je:" << endl;
+        if (izbor == 2)
+        {
+            for (int i = 0; i < ovisnik; i++)
+            {
+                cout << "Provjera vaseg racuna:" << endl;
+                cout << "\t\t" << Imeprezime[i] << endl;
+                cout << "\t\t" << ulog[i] << endl;
+                cout << "\t\t"<<"da li je ovo vas racun koji ste registrirali ? (da / ne)" << endl;
+                cout << "\t\t" << endl;
+                cin >> odgovor;
+                if (odgovor == "da" || odgovor == "Da" || odgovor == "DA")
+                    cout << "\t\t" << "Odlicno, odaberite igru. " << endl;
+                else if (odgovor == "ne" || odgovor == "Ne" || odgovor == "NE")
+                    cout << "\t\t" << "pokusajte se ponovo registrirati." << endl;
+                cout << endl; 
+            }
 
-     }
-     if (izbor == 3) {
-         cout << "Odabrali ste utrke èetveronožnih trèkala" << endl;
-         cout << "PRAVILA: Èetveronožna trèkala trèe s desna na lijevo." <<endl <<"Pobjednik je èetveronožno trèkalo koje prvo doðe do završne linije." << endl "Ako vaše èetveronožno trèkalo pobijedi dobitak je 1,5x vašeg uloga.";
-         cout << "Odaberite jedno Èetveronožno Trèkalo:";
+        }
+        if (izbor == 3) {
+            cout << "Odabrali ste utrke èetveronožnih trèkala" << endl;
+            cout << "PRAVILA: Èetveronožna trèkala trèe s desna na lijevo." <<endl <<"Pobjednik je èetveronožno trèkalo koje prvo doðe do završne linije." << endl "Ako vaše èetveronožno trèkalo pobijedi dobitak je 1,5x vašeg uloga.";
+            cout << "Odaberite jedno Èetveronožno Trèkalo:";
 
-     }
+    }
+}
      if (izbor == 4) {
          cout << "Odabrali ste Nogometne Utakmice:" << endl;
          cout << "PRAVILA:"<<endl<<endl "Na poèetku izlaze dva nogometna kluba koje æe odigrati utakmicu." <<endl << "Odabrani klub je pobijedio - dupli dobitak uloga." << endl "U suprotnom (gubitak ili izjednaèeno) gubite svotu uloga.";
